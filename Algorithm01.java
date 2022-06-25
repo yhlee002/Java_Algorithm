db07d2c8920c1156ce9837b751fa5c45ec820a02
yhlee <xxxoxxo002@gmail.com>
2022-06-25 23:25:54 +0900

    JAVA_Algorithm project initial commit

---
+++ Algorithm01.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+// 해당 문자열이 포함된 수 찾기
+public class algorithm01 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input1 = in.nextLine();
+        String input2 = in.nextLine();
+
+        int len = input2.length();
+        int cnt = 0;
+        for (int i = 0; i <= input1.length() - len; i++) {
+            String s1 = input1.toLowerCase();
+            String s2 = input2.toLowerCase();
+            if (s1.substring(i, i + len).equals(s2)) cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
---
+++ Algorithm02.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+// 대소문자 변경
+public class Algorithm02 {
+    public static void main(String[] args) {
+        // A : 65, a : 97
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if ((byte) chars[i] >= 97) chars[i] -= 32;
+            else chars[i] += 32;
+        }
+
+        System.out.println(new String(chars));
+    }
+}
