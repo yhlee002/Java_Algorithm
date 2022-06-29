570283c99716e5e7a86d7fcb9c44181b1ec8d6a6
yhlee <xxxoxxo002@gmail.com>
2022-06-29 20:48:42 +0900

    12 추가
    + Integer.parseInt(String str, int radix)를 통해 'radix'진수 값을 10진수로 변환 가능

---
+++ Algorithm/Algorithm12.java
@@ -0,0 +1,29 @@
+package Algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+// #을 1로, *을 0으로
+public class Algorithm12 {
+    private String solution(String str) {
+        String answer = "";
+        for (int i = 0; i < str.length(); i += 7){
+            String s = str.substring(i, i + 7);
+            s = s.replace("#", "1").replace("*", "0");
+            int num = Integer.parseInt(s, 2);
+            char c = (char) num;
+            answer += c;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algorithm12 al = new Algorithm12();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        System.out.println(al.solution(str));
+    }
+}
