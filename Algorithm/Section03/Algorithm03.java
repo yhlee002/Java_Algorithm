fb9cd4b54f00b6ebd9fcc2060fd88c2e079069dc
yhlee <xxxoxxo002@gmail.com>
2022-07-05 22:47:00 +0900

    섹션3의 03번 수정(설명 참고)
    불필요한 변수 제거, 인덱스를 좀 더 간단하게 수정('i - 1 ~ i + k - 1'에서 'i - k ~ i'로 변경)

--- Algorithm/Section03/Algorithm03.java
+++ Algorithm/Section03/Algorithm03.java
@@ -3,25 +3,21 @@ package Algorithm.Section03;
 import java.util.*;
 /*
     N일 동안의 매출 중 연속 K일 동안의 최대 매출액 출력
-    첫 줄에 N일, K일 입력
+    첫 줄에 N(5 <= N <= 100000)일, K(2 <= k <= N)일 입력
     다음 줄에 N일 간의 일일 매출액 입력
  */
 public class Algorithm03 {
     private int solution(int n, int k,  int[] arr) {
         int answer = 0;
-        int preSum = 0;
-        for (int i = 0; i < k; i++) {
-            preSum += arr[i];
+        int sum = 0;
+        for (int i = 0; i < k; i++) sum += arr[i];
+        answer = sum;
+
+        for (int i = k; i < n; i++) {
+            sum = sum - arr[i - k] + arr[i];
+            if (answer < sum) answer = sum;
         }
 
-        int max = preSum;
-        for (int i = 1; i < n - k + 1; i++) {
-            int sum = preSum - arr[i - 1] + arr[i + k - 1];
-            preSum = sum;
-            if (max < sum) max = sum;
-        }
-
-        answer = max;
         return answer;
     }
 
