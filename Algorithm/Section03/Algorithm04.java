6914b00c7034fce0491e91dfe17cf38fdebbff0b
yhlee <xxxoxxo002@gmail.com>
2022-07-06 00:39:42 +0900

    섹션3의 04번 수정(2번째 방법 - 기존 코드의 중복 제거)

--- Algorithm/Section03/Algorithm04.java
+++ Algorithm/Section03/Algorithm04.java
@@ -4,20 +4,14 @@ import java.util.Scanner;
 
 public class Algorithm04 {
     private int solution(int n, int m, int[] arr) {
-        int answer = 0;
-        int lt = 0, rt = 0;
-
-        int sum = arr[0];
-        while (lt <= rt && rt < n) {
-            if (sum < m) {
-                if (rt == n - 1) break;
-                else sum += arr[++rt];
-            }
-            else if (sum > m) sum -= arr[lt++];
-            else {
-                answer++;
-                if (rt == n - 1) break;
-                else sum += arr[++rt];
+        int answer = 0, lt = 0;
+        int sum = 0;
+        for (int rt = 0; rt < n; rt++) {
+            sum += arr[rt];
+            if (sum == m) answer++;
+            while(sum > m) {
+                sum -= arr[lt++];
+                if (sum == m) answer++;
             }
         }
 
