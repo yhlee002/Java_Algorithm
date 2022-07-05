ed028da392001c7e909d055381abd154985503b2
yhlee <yhlee@haebomtech.com>
2022-07-06 08:10:20 +0900

    섹션3의 04번 주석 추가

--- Algorithm/Section03/Algorithm04.java
+++ Algorithm/Section03/Algorithm04.java
@@ -2,6 +2,12 @@ package Algorithm.Section03;
 
 import java.util.Scanner;
 
+/*
+    N개의 수로 이루어진 수열 입력
+    이 수열에서 연속부분수열의 합이 특정숫자 M이 되는 경우가 몇 번 있는지 출력
+    첫 줄에 수열의 길이 N과 정수 M 입력
+    다음 줄에 수열 입력
+ */
 public class Algorithm04 {
     private int solution(int n, int m, int[] arr) {
         int answer = 0, lt = 0;
