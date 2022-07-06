937506f697fd983ff4db0aa8687b062803e2509d
yhlee <xxxoxxo002@gmail.com>
2022-07-06 22:37:37 +0900

    섹션3의 05번 버전 2 추가(수학적 방식)
    cf. 기존 방식(버전 1) : two-pointer 알고리즘 방식

--- Algorithm/Section03/Algorithm05.java
+++ Algorithm/Section03/Algorithm05.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class Algorithm05 {
     private int solution(int num) {
         int answer = 0;
+        /* 첫 번째 방식(two-pointer 방식)
         int m = num/2 + 1;
         int[] arr = new int[m];
         for(int i = 0; i < arr.length; i++) arr[i] = i + 1;
@@ -20,6 +21,17 @@ public class Algorithm05 {
                 sum -= arr[lt++];
                 if (sum == num) answer++;
             }
+        }
+         */
+
+        // 두 번째 방식(수학적 방식)
+        int cnt = 1;
+        num--; // 연속 1 번째 수에 1 부여
+        while (num > 0) {
+            cnt++; // 2, 3, 4, ... 부여(연속 m 번째 수)
+            num = num - cnt; // 2, 3, 4, ... 개의 수로 나누어 떨어지는지 여부 확인
+            if (num % cnt == 0) answer++;
+
         }
 
         return answer;
