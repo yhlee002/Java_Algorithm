ebf5436cd01a385f4c235fa2091836d9fddd0fe5
yhlee <xxxoxxo002@gmail.com>
2022-08-09 19:17:16 +0900

    섹션8의 9번 문제 수정(개선)
    오름차순으로 출력하는 부분을 배열을 정렬시켜 해싱하는 방식에서 재귀 함수에 들어온 값보다 큰 수들로 재귀함수를 호출하는 방식으로 변경(시간 효율성, 공간효율성 증가)

--- Algorithm/Section08/Algorithm09.java
+++ Algorithm/Section08/Algorithm09.java
@@ -5,30 +5,19 @@ import java.util.*;
 public class Algorithm09 {
     static int n;
     static int m;
-    static boolean[] checked;
+//    static boolean[] checked;
     static int[] result;
-    static Set<String> set;
+//    static Set<String> set;
 
 
     void DFS (int L, int start) {
         if (L == m) {
-            String str = "";
-            int[] arr = Arrays.stream(result).sorted().toArray();
-            for (int i = 0; i < m; i++) {
-                str += arr[i] + " ";
-            }
-            if (!set.contains(str)) {
-                set.add(str);
-                System.out.println(str);
-            }
+            for (int x : result) System.out.print(x + " ");
+            System.out.println();
         } else {
             for (int i = start; i <= n; i++) {
-                if (!checked[i]) {
-                    checked[i] = true;
-                    result[L] = i;
-                    DFS(L + 1, start);
-                    checked[i] = false;
-                }
+                result[L] = i;
+                DFS(L + 1, i + 1);
             }
         }
     }
@@ -39,11 +28,8 @@ public class Algorithm09 {
         n = sc.nextInt();
         m = sc.nextInt();
         result = new int[m];
-        checked = new boolean[n + 1];
-        set = new HashSet<>();
 
-        for (int i = 1; i <= n; i++) {
-            main.DFS(0, i);
-        }
+        main.DFS(0, 1);
+
     }
 }
