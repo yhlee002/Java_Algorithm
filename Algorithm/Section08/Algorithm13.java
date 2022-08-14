2c1690fa2af062a6a72941728bd3f1bc2e9a7e3d
yhlee <xxxoxxo002@gmail.com>
2022-08-14 12:50:48 +0900

    섹션8의 13번 문제 개선(DFS 공간효율성 개선)

--- Algorithm/Section08/Algorithm13.java
+++ Algorithm/Section08/Algorithm13.java
@@ -6,16 +6,15 @@ import java.util.Scanner;
 public class Algorithm13 {
     static int n;
     static int[][] matrix;
-    static boolean[][] checked;
     static int[] ym = {-1, -1, 0, 1, 1, 1, 0, -1};
     static int[] xm = {0, 1, 1, 1, 0, -1, -1, -1};
     static int cnt= 0;
 
     void DFS() {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (!checked[i][j] && matrix[i][j] == 1) {
-                    checked[i][j] = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1) {
+                    matrix[i][j] = 0;
                     DFS(i, j);
                     cnt++;
                 }
@@ -27,9 +26,9 @@ public class Algorithm13 {
         for (int i = 0; i < 8; i++) {
             int my = y + ym[i];
             int mx = x + xm[i];
-            if (my > 0 && mx > 0 && my <= n && mx <= n) {
-                if (!checked[my][mx] && matrix[my][mx] == 1) {
-                    checked[my][mx] = true;
+            if (my >= 0 && mx >= 0 && my < n && mx < n) {
+                if (matrix[my][mx] == 1) {
+                    matrix[my][mx] = 0;
                     DFS(my, mx);
                 }
             }
@@ -40,16 +39,15 @@ public class Algorithm13 {
         Algorithm13 main = new Algorithm13();
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        matrix = new int[n + 1][n + 1];
-        checked = new boolean[n + 1][n + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+        matrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 matrix[i][j] = sc.nextInt();
             }
         }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 main.DFS();
             }
         }
